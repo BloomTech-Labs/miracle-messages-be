610268f50aa151387e59c5856a41a67dad1ddc9c
Thierry Joux <tjoux@hotmail.com>
2019-07-12 15:38:55 -0700

    get users endpoint

--- server.js
+++ server.js
@@ -9,4 +9,18 @@ server.get('/', (req,res) => {
     res.status(200).json({hello: 'World!'});
 })
 
+const db = require('./config/dbConfig');
+
+function find() {
+    return db('users');
+}
+
+server.get('/users', (req, res) => {
+    find()
+        .then(user => res.status(200).json(user) )
+        .catch(
+            err => res.status(500).json({errorMessage: err})
+        )
+} )
+
 module.exports = server;
