cf475a86088a4a8ca292f6cc41f35f5e1b5b2fac
Thierry Joux <tjoux@hotmail.com>
2019-07-17 09:10:44 -0700

    changing connection string for staging for testing

--- knexfile.js
+++ knexfile.js
@@ -6,8 +6,7 @@ require('dotenv').config()
 module.exports = {
   staging: {
     client: 'pg',
-    connection: process.env.HEROKU_POSTGRESQL_BLACK_URL,
-
+    connection: 'postgres://fjqaiuqrszutzf:9bf95beffb14b87dee814a29f73f303f05aa329ac8d2fecd484ecc166e61fcb9@ec2-54-83-1-101.compute-1.amazonaws.com:5432/d1h1cg5bjnl52m',
     useNullAsDefault: true,
 
     migrations: {
