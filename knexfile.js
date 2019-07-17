0d590aca708e6b87f78a8f459d5c783e06dcd7ca
Thierry Joux <tjoux@hotmail.com>
2019-07-17 09:16:35 -0700

    revert back previous commit - changed back connection string for staging

--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,8 @@ require('dotenv').config()
 module.exports = {
   staging: {
     client: 'pg',
-    connection: 'postgres://fjqaiuqrszutzf:9bf95beffb14b87dee814a29f73f303f05aa329ac8d2fecd484ecc166e61fcb9@ec2-54-83-1-101.compute-1.amazonaws.com:5432/d1h1cg5bjnl52m',
+    connection: process.env.HEROKU_POSTGRESQL_BLACK_URL,
+
     useNullAsDefault: true,
 
     migrations: {
