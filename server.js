adcd35d7fc5fea1c13da0ceca6fe8d9c1c1a5f82
Navid Karami <navid.gsz@gmail.com>
2019-07-18 13:41:48 -0700

    added basic testing

--- server.js
+++ server.js
@@ -5,23 +5,20 @@ server.use(express.json());
 const cors = require('cors');
 server.use(cors());
 
-server.get('/', (req,res) => {
-    res.status(200).json({hello: 'World!'});
-})
+server.get('/', (req, res) => {
+  res.status(200).json({ hello: 'World!' });
+});
 
 const db = require('./config/dbConfig');
 
 function find() {
-    return db('users');
+  return db('users');
 }
 
 server.get('/users', (req, res) => {
-    find()
-        .then(user => res.status(200).json(user) )
-        .catch(
-            err => res.status(500).json({errorMessage: err})
-        )
-} )
-//test test
+  find()
+    .then(user => res.status(200).json(user))
+    .catch(err => res.status(500).json({ errorMessage: err }));
+});
 
 module.exports = server;
