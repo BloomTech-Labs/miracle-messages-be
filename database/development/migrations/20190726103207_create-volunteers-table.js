14878a5662a935ca07cd969525e5668ddff84303
Navid Karami <navid.gsz@gmail.com>
2019-07-26 11:15:24 -0700

    crated volunteers table

---
+++ database/development/migrations/20190726103207_create-volunteers-table.js
@@ -0,0 +1,20 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('volunteers', tbl => {
+    tbl.increments();
+    tbl.string('fname', 128).notNullable();
+    tbl.string('lname', 128).notNullable();
+    tbl
+      .string('email', 128)
+      .notNullable()
+      .unique();
+    tbl.string('phone', 32).unique();
+    tbl.string('city', 64).notNullable();
+    tbl.string('state', 32).notNullable();
+    tbl.string('country', 32).notNullable();
+    tbl.string('comment', 1024);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('volunteers');
+};
