30eb42dff8d55b6d9a4e61bbb74b0392dbe106f2
Greyflanel <richardlovelace811@gmail.com>
2019-08-05 15:51:44 -0500

    adding authentication

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const server = express();
 server.use(express.json());
-
+const helmet = require('helmet')
 const chaptersRouter = require('./api/chapterRouter.js');
 const formRouter = require('./api/formRouter.js');
 
 const cors = require('cors');
+const userRouter = require('./api/usersRouter')
 
+server.use(helmet());
 server.use(cors());
 
 server.get('/', (req, res) => {
@@ -15,5 +17,6 @@ server.get('/', (req, res) => {
 
 server.use('/api/chapter', chaptersRouter);
 server.use('/api/form', formRouter);
+server.use('/api/user', userRouter);
 
 module.exports = server;
