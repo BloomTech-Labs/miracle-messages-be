5c7ac9f3d32f935f0bb374d120af93734e55bde1
azrap <azraap@gmail.com>
2019-08-07 09:09:53 -0700

    added alidation for phone number

---
+++ database/testing/migrations/20190729075101_create-volunteer-testing-table.js.js
@@ -0,0 +1,22 @@
+
+exports.up = async function(knex, Promise) {
+  await knex.schema.createTable('volunteers', tbl => {
+      tbl.increments();
+      tbl.string('fname', 128).notNullable();
+      tbl.string('lname', 128).notNullable();
+      tbl
+        .string('email', 128)
+        .notNullable()
+        .unique();
+      tbl.string('phone', 32).unique();
+      tbl.string('city', 64).notNullable();
+      tbl.string('state', 32).notNullable();
+      tbl.string('country', 32).notNullable();
+      tbl.text('comment');
+    });
+  };
+  
+  exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('volunteers');
+  };
+  
