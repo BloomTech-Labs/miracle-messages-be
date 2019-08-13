929c67f003aea41c5b581df46ca033e388c6bd37
Greyflanel <richardlovelace811@gmail.com>
2019-08-13 13:25:39 -0500

    Got rid of all cypress and reverted back to jest. Have a few new tests, but not done yet.

--- database/testing/form.test.js
+++ database/testing/form.test.js
@@ -4,10 +4,7 @@ const server = require("../../server");
 const db = require("../../config/dbConfig.js");
 const Volunteers = require("../../models/form-model.js");
 
-describe("volunteers model", () => {
-  beforeEach(async () => {
-    await db("volunteers");
-  });
+
 
   describe("GET /", () => {
     it("should return 200", async () => {
@@ -19,31 +16,42 @@ describe("volunteers model", () => {
       const res = await request(server).get("/api/form");
       expect(res.type).toBe("application/json");
     });
+})
 
-    it('should insert volunteers into the db', async () => {
-      await Volunteers.addVolunteer(({ fname: 'John',
-      lname: 'Smith',
-      email: 'john@smith456.com',
-      phone: '+14802658966',
-      city: 'Los Angeles',
-      state: 'CA',
-      country: 'United States',
-      comment: 'No comment'
+describe("volunteers model", () => {
+  beforeEach(async () => {
+    await db("volunteers").del();
+  });
+    describe('post /', () => {
+      it('should insert volunteers into the db', async () => {
+      await Volunteers.addVolunteer(({fname: "John2",
+      lname: "Smith",
+      email: "jsmith@.com",
+      phone: "+14802658966",
+      city: "Los Angeles",
+      state: "CA",
+      country: "United States",
+      comment: "No comment",
+     
+      
       }));
-      await Volunteers.addVolunteer({ fname: 'John2',
-      lname: 'Smith',
-      email: 'john@smith789.com',
-      phone: '+14802658966',
-      city: 'Los Angeles',
-      state: 'CA',
-      country: 'United States',
-      comment: 'No comment' });
-
+      await Volunteers.addVolunteer({  fname: "John",
+      lname: "Smith",
+      email: "johnsmith@.com",
+      phone: "+14802658966",
+      city: "Los Angeles",
+      state: "CA",
+      country: "United States",
+      comment: "No comment",
+     
+      });
       const volunteers = await db('volunteers')
 
        expect(volunteers).toHaveLength(2);
-       expect(volunteers[0].location).toBe('San Antonio, TX');
+       expect(volunteers[0].fname).toBe("John2");
+       expect(volunteers[1].email).toContain("@");
        
+    })  
    });
 })
-})
+
