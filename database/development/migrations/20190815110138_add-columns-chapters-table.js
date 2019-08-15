34cbc2d08eefb04613d561c6dda54bb6cc925ca0
Thierry Joux <tjoux@hotmail.com>
2019-08-15 13:08:20 -0700

    create new chapter table migration and seeding

---
+++ database/development/migrations/20190815110138_add-columns-chapters-table.js
@@ -0,0 +1,28 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.table('chapters', table => {
+        table.string('state', 2);
+        table.renameColumn('location', 'city')
+        table.integer('numreunions')
+        table.string('chapter_img_url', 512);
+        table.string('reunion_img_url', 512)
+        table.string('established_date');
+        table.string('description', 2048);
+        table.string('story', 2048);
+        table.string('email')
+    })
+  };
+  
+  exports.down = function(knex, Promise) {
+    return knex.schema.table('chapters', table => {
+      table.dropColumn('state')
+      table.renameColumn('city', 'location')
+      table.dropColumn('numreunions')
+      table.dropColumn('chapter_img')
+      table.dropColumn('reunion_img')
+      table.dropColumn('established_date')
+      table.dropColumn('description')
+      table.dropColumn('story')
+      table.dropColumn('email')
+    })
+  };
