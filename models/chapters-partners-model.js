6688448e43740140d846e30ed939f97fc11c4302
Thierry Joux <tjoux@hotmail.com>
2019-08-20 10:49:12 -0700

    fixed knex database function to get partners of a chapter

--- models/chapters-partners-model.js
+++ models/chapters-partners-model.js
@@ -4,6 +4,9 @@ module.exports = {
     removeChapterPartner,
 }
 
+/****************************************************************************/
+/*                      Remove all partners of a chapter                    */
+/****************************************************************************/
 function removeChapterPartner(id) {
     return dB('chapters_partners')
         .where({"partnersid": id})
--- models/partners-model.js
+++ models/partners-model.js
@@ -5,15 +5,21 @@ module.exports = {
     remove,
 }
 
+/****************************************************************************/
+/*                        Find all partners of a chapter                    */
+/****************************************************************************/
 function find(id) {
+    console.log(id)
     return db.select('partners.name', 'partners.site_url', 'partners.icon_url')
         .from ('chapters_partners')
         .innerJoin('partners', 'chapters_partners.partnersid', 'partners.id')
-        .where({id})
+        .where({'chapters_partners.chaptersid': id})
 }
 
+/****************************************************************************/
+/*                              Remove a partner                            */
+/****************************************************************************/
 function remove(id) {
-    console.log('in model')
     return db('partners')
         .where({id})
         .del();
