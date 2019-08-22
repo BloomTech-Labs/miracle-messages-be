a301c75f7e1e2bff1395879314f30b0054d70e36
azrap <azraap@gmail.com>
2019-08-22 11:59:21 -0700

    the put for the partners is working

---
+++ helperFunctions/postImageGetUrl.js
@@ -0,0 +1,23 @@
+const uploadToS3 = require("./uploadToS3.js");
+const aws_link =
+  "https://labs14-miracle-messages-image-upload.s3.amazonaws.com/";
+
+const postImageGetUrl = async function(img, res) {
+  //pulling out the chapter_img file
+  console.log("in the function");
+  //uploading the file to AWS
+  try {
+    uploadToS3(img);
+    console.log("success");
+  } catch (error) {
+    res.status(500).json({ error: `error uploading the ${img} to AWS` });
+  }
+
+  // storing the chapter image url i database
+
+  const encodedImgName = encodeURI(await img.name);
+
+  return aws_link + encodedImgName;
+};
+
+module.exports = postImageGetUrl;
