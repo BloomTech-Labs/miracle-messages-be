8a30ac7ab97f0b13cfc477439243b42e8487778c
Thierry Joux <tjoux@hotmail.com>
2019-08-29 13:21:27 -0700

    Modified partner middleware to get rid of unhandledPromiseRejectionWarning

--- middleware/partnersMW.js
+++ middleware/partnersMW.js
@@ -30,33 +30,73 @@ async function validatePartnerId(req, res, next) {
 
 
 async function verifyPartnerData(req, res, next) {
+
     const aws_link = "https://labs14-miracle-messages-image-upload.s3.amazonaws.com/";
     const {category, name, site_url} = req.body;
 
-
-    console.log(req.body);
-
-    if(!category || !name || !site_url) {
-        res.status(400).json({errorMessage: "All partner fields are required"});
-    }
-    
-    if (!req.files || !req.files.partner_icon) {
-        res.status(400).json({errorMessage: "image file is needed"});
-    }
-
-    const iconURL = aws_link + encodeURI(req.files.partner_icon.name);
-
-    try {
-        const found = await db('partners').where({icon_url:iconURL});
-        if(found.length == 0) {
-            next()
+    if(category && name && site_url) {
+        if(req.files && req.files.partner_icon) {
+            try {   
+                    const iconURL = aws_link + encodeURI(req.files.partner_icon.name);
+                    const found = await db('partners').where({icon_url:iconURL});
+                    if(found.length == 0) {
+                        next()
+                    }
+                    else {
+                        res.status(409).json({errorMessage: "An icon image with this filename already exists"});
+                    }
+                }
+            catch {
+                res.status(500).json({errorMessage: " There is a problem checking if icon url exists in database"});
+            }
         }
         else {
-            res.status(409).json({errorMessage: "An icon image with this filename already exists"});
+            res.status(400).json({errorMessage: "image file with partner_icon property is required"})
         }
     }
-    catch {
-        res.status(500).json({errorMessage: " There is a problem checking if icon url exists in database"});
+    else {
+        res.status(400).json({errorMessage: "All partner fields are required"});
     }
+
+    // if(!category || !name || !site_url) {
+    //     res.status(400).json({errorMessage: "All partner fields are required"});
+    // }
+    // console.log(!req.files);
+
+    // try {
+    //     if(!req.files.partner_icon)
+    //         res.status(400).json({errorMessage: "no partner_icon property in image file"})
+    // }
+    // catch {
+    //     res.status(400).json({errorMessage: "no image file"});
+    // }
+
+    // // if (req.files) {
+
+    // //     try {
+    // //         if(!req.file.partner_icon)
+    // //             res.status(400).json({errorMessage: "no partner_icon property in image file"});
+    // //     }
+    // //     catch {
+
+    // //     }
+    // // else {
+    // //     res.status(400).json({errorMessage: "no image file"});
+    // // }
+
+    // const iconURL = aws_link + encodeURI(req.files.partner_icon.name);
+
+    // try {
+    //     const found = await db('partners').where({icon_url:iconURL});
+    //     if(found.length == 0) {
+    //         next()
+    //     }
+    //     else {
+    //         res.status(409).json({errorMessage: "An icon image with this filename already exists"});
+    //     }
+    // }
+    // catch {
+    //     res.status(500).json({errorMessage: " There is a problem checking if icon url exists in database"});
+    // }
 
 }
