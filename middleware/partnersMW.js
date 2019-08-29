7256ad8b1ac5db800bf3fe6fd25bf5435fcadc7b
Thierry Joux <tjoux@hotmail.com>
2019-08-29 11:20:08 -0700

    Created middleware to check partner post fields

--- middleware/partnersMW.js
+++ middleware/partnersMW.js
@@ -2,6 +2,7 @@ const db = require('../config/dbConfig.js')
 
 module.exports = {
     validatePartnerId,
+    verifyPartnerData,
 }
 
 
@@ -23,3 +24,39 @@ async function validatePartnerId(req, res, next) {
     }
 }
 
+/****************************************************************************/
+/*  Verify that all partner fields exist with no duplicate image filenames  */
+/****************************************************************************/
+
+
+async function verifyPartnerData(req, res, next) {
+    const aws_link = "https://labs14-miracle-messages-image-upload.s3.amazonaws.com/";
+    const {category, name, site_url} = req.body;
+
+
+    console.log(req.body);
+
+    if(!category || !name || !site_url) {
+        res.status(400).json({errorMessage: "All partner fields are required"});
+    }
+    
+    if (!req.files || !req.files.partner_icon) {
+        res.status(400).json({errorMessage: "image file is needed"});
+    }
+
+    const iconURL = aws_link + encodeURI(req.files.partner_icon.name);
+
+    try {
+        const found = await db('partners').where({icon_url:iconURL});
+        if(found.length == 0) {
+            next()
+        }
+        else {
+            res.status(409).json({errorMessage: "An icon image with this filename already exists"});
+        }
+    }
+    catch {
+        res.status(500).json({errorMessage: " There is a problem checking if icon url exists in database"});
+    }
+
+}
