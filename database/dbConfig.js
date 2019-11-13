1a078583c081be8fb6f42404aefe60d7004b7788
raberin <rmvaberin@gmail.com>
2019-11-13 11:34:06 -0800

    Removed redundant dbconfig file

--- database/dbConfig.js
+++
@@ -1,8 +0,0 @@
-const knex = require("knex");
-
-const config = require("../knexfile.js");
-
-//Test variable only set when yarn test is run
-const dbEnv = process.env.DB_ENV || "development";
-
-module.exports = knex(config[dbEnv]);
