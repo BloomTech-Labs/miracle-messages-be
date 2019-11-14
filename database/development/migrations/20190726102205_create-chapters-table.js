eabdbdb04f288518710905e7f53f5e525530324f
raberin <rmvaberin@gmail.com>
2019-11-14 13:48:09 -0800

    Copied staging-server chapters migration info

--- database/development/migrations/20190726102205_create-chapters-table.js
+++ database/development/migrations/20190726102205_create-chapters-table.js
@@ -1,10 +1,22 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('chapters', tbl => {
     tbl.increments();
-    tbl.string('location', 128).notNullable();
+  
     tbl.integer('numvolunteers').notNullable();
     tbl.double('longitude').notNullable();
     tbl.double('latitude').notNullable();
+    tbl.string('city',128).notNullable();
+    tbl.string('title',128).notNullable();
+    tbl.string('state',128).notNullable();
+    tbl.integer('numreunions').notNullable();
+    tbl.integer('msg_recorded').notNullable();
+    tbl.integer('msg_delivered').notNullable();
+    tbl.string('chapter_img_url',128).notNullable();
+    tbl.string('reunion_img_url',128).notNullable();
+    tbl.string('established_date',128).notNullable();
+    tbl.text('description').notNullable();
+    tbl.text('story').notNullable();
+    
   });
 };
 
