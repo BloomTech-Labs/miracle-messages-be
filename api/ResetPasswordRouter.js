bff1fcf08a289f9817104973c55446a171d8114b
noahfranco <noahfra@gmail.com>
2019-11-20 18:34:30 -0500

    added server.use and imported from ResetPasswordRouter into server.js

---
+++ api/ResetPasswordRouter.js
@@ -0,0 +1,13 @@
+const express = require("express"); 
+const router = express.Router(); 
+
+// To find the req.body of id and email to make sure that the email existis in the database 
+router.get("/", (req, res) => {
+
+})
+
+
+// To replace volunteers old password with new one 
+router.put("/", (req, res) => {
+
+})
