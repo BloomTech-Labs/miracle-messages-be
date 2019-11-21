76ed141fdfdd2e833e17e8e3b3c227e7906a983c
noahfranco <noahfra@gmail.com>
2019-11-20 19:03:54 -0500

    created get request for email

--- api/ResetPasswordRouter.js
+++ api/ResetPasswordRouter.js
@@ -1,9 +1,25 @@
 const express = require("express"); 
 const router = express.Router(); 
 
-// To find the req.body of id and email to make sure that the email existis in the database 
-router.get("/", (req, res) => {
+const Volunteer = require("../models/volunteer-model.js"); 
 
+// To find the req.body of id and email to make sure that the email exists in the database 
+router.get("/email", (req, res) => {
+    const { id } = req.body;
+    const { email } = req.body;
+
+    Volunteer.findBy(id, email)
+    .then(mail => {
+        if(!id && !email) {
+            res.status(204).json(console.log("No Content"), mail)
+        } else {
+            res.status(200).json(mail)
+        }
+    })
+    .catch(error => {
+        console.log("Catch", error)
+        res.status(500).json(error)
+    })
 })
 
 
