75817f5bc7c318818fe22c2d94496d337efd3eb3
raberin <rmvaberin@gmail.com>
2019-12-02 10:37:13 -0800

    Changed hard-coded port to dynamic for deployment

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-require('dotenv').config();
-const server = require('./server');
+require("dotenv").config();
+const server = require("./server");
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 server.listen(port, () => console.log(`API running on port ${port}`));
