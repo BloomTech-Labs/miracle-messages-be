1e83d7f3f43bc99bb14f0fb129de9b7615e9d21e
raberin <rmvaberin@gmail.com>
2019-12-03 09:14:43 -0800

    Added boolean row to chapters table

--- database/development/migrations/20190726102205_create-chapters-table.js
+++ database/development/migrations/20190726102205_create-chapters-table.js
@@ -1,26 +1,28 @@
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('chapters', tbl => {
+  return knex.schema.createTable("chapters", tbl => {
     tbl.increments();
-  
-    tbl.integer('numvolunteers').notNullable();
-    tbl.double('longitude').notNullable();
-    tbl.double('latitude').notNullable();
-    tbl.string('city',128).notNullable();
-    tbl.string('title',128).notNullable();
-    tbl.string('state',128).notNullable();
-    tbl.integer('numreunions').notNullable();
-    tbl.integer('msg_recorded').notNullable();
-    tbl.integer('msg_delivered').notNullable();
-    tbl.string('chapter_img_url').notNullable();
-    tbl.string('reunion_img_url').notNullable();
-    tbl.string('established_date').notNullable();
-    tbl.text('description').notNullable();
-    tbl.text('story').notNullable();
-    tbl.string('email').notNullable();
-    
+
+    tbl.integer("numvolunteers").notNullable();
+    tbl.double("longitude").notNullable();
+    tbl.double("latitude").notNullable();
+    tbl.string("city", 128).notNullable();
+    tbl.string("title", 128).notNullable();
+    tbl.string("state", 128).notNullable();
+    tbl.integer("numreunions").notNullable();
+    tbl.integer("msg_recorded").notNullable();
+    tbl.integer("msg_delivered").notNullable();
+    tbl.string("chapter_img_url").notNullable();
+    tbl.string("reunion_img_url").notNullable();
+    tbl.string("established_date").notNullable();
+    tbl.text("description").notNullable();
+    tbl.text("story").notNullable();
+    tbl.string("email").notNullable();
+
+    //Pending Requests
+    tbl.boolean("approved").defaultTo(false);
   });
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTableIfExists('chapters');
+  return knex.schema.dropTableIfExists("chapters");
 };
