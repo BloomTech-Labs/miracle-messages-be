2d315af8823728fe721cde40a621aee57b48eed4
raberin <rmvaberin@gmail.com>
2019-12-10 15:05:30 -0800

    Added delete and add routes for chapters-volunteers

--- api/volunteersRouter.js
+++ api/volunteersRouter.js
@@ -4,11 +4,12 @@ const router = express.Router();
 // const MW = require("../middleware/partnersMW");
 const volunteersDb = require("../models/volunteer-model");
 const chaptersVolunteersDb = require("../models/chapters-volunteers-model");
+const authenticated = require("../auth/restricted-middleware");
 
 /****************************************************************************/
 /*                 Get all volunteers 
 /****************************************************************************/
-router.get("/", async (req, res) => {
+router.get("/", authenticated, async (req, res) => {
   try {
     const volunteers = await volunteersDb.find();
     res.status(200).json(volunteers);
@@ -21,7 +22,7 @@ router.get("/", async (req, res) => {
 /****************************************************************************/
 /*                 Get all volunteers of one specific chapter                 */
 /****************************************************************************/
-router.get("/:id", async (req, res) => {
+router.get("/:id", authenticated, async (req, res) => {
   const chapterId = req.params.id;
   try {
     const volunteers = await volunteersDb.findById(chapterId);
@@ -33,13 +34,58 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+//**********************************************************************
+//********* SIGNING UP AS A VOLUNTEER TO A CHAPTER   *************/
+//**********************************************************************
+
+router.post("/:id/volunteers", authenticated, async (req, res) => {
+  try {
+    console.log("in the router");
+    console.log(req.user_id);
+    console.log(req.params.id);
+
+    const id = await chaptersVolunteersDb.assignChapterVolunteer(
+      req.user_id,
+      req.params.id
+    );
+
+    res.status(200).json(id);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "error assigning zee dang partner to the chapter" });
+  }
+});
+
 /****************************************************************************/
-/*      Delete a volunteer from a specific chapter
+/*      Delete a volunteer from a specific chapter - Admin
 /****************************************************************************/
-router.delete("/:id/volunteers/:volunteerid", async (req, res) => {
+router.delete(
+  "/:id/volunteers/:volunteerid",
+  authenticated,
+  async (req, res) => {
+    try {
+      const count = await chaptersVolunteersDb.removeSpecificChapterVolunteer(
+        req.params.volunteerid,
+        req.params.id //chapterId
+      );
+
+      res.status(200).json(count);
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: "error unassigning zee dang partner from the chapter" });
+    }
+  }
+);
+
+/****************************************************************************/
+/*      Delete a volunteer from a specific chapter - Volunteer
+/****************************************************************************/
+router.delete("/:id/volunteers/", authenticated, async (req, res) => {
   try {
     const count = await chaptersVolunteersDb.removeSpecificChapterVolunteer(
-      req.params.volunteerid,
+      req.user_id,
       req.params.id //chapterId
     );
 
@@ -47,7 +93,7 @@ router.delete("/:id/volunteers/:volunteerid", async (req, res) => {
   } catch (error) {
     res
       .status(500)
-      .json({ error: "error unassigning zee dang partner from the chapter" });
+      .json({ error: "error unassigning zee dang volunteer from the chapter" });
   }
 });
 
