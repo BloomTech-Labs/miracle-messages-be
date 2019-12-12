a88290816a4f5a794049220a42fb18e3a854866d
raberin <rmvaberin@gmail.com>
2019-12-12 09:38:51 -0800

    Removed notNullable() in some rows

--- database/development/migrations/20190726102205_create-chapters-table.js
+++ database/development/migrations/20190726102205_create-chapters-table.js
@@ -2,20 +2,20 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable("chapters", tbl => {
     tbl.increments();
 
-    tbl.integer("numvolunteers").notNullable();
+    tbl.integer("numvolunteers");
     tbl.double("longitude").notNullable();
     tbl.double("latitude").notNullable();
     tbl.string("city", 128).notNullable();
     tbl.string("title", 128).notNullable();
     tbl.string("state", 128).notNullable();
-    tbl.integer("numreunions").notNullable();
-    tbl.integer("msg_recorded").notNullable();
-    tbl.integer("msg_delivered").notNullable();
+    tbl.integer("numreunions");
+    tbl.integer("msg_recorded");
+    tbl.integer("msg_delivered");
     tbl.string("chapter_img_url").notNullable();
-    tbl.string("reunion_img_url").notNullable();
+    tbl.string("reunion_img_url");
     tbl.string("established_date").notNullable();
     tbl.text("description").notNullable();
-    tbl.text("story").notNullable();
+    tbl.text("story");
     tbl.string("email").notNullable();
     tbl.string("facebook");
 
