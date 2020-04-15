d10f32ba01504338ac443cb75dfc530e16930492
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2020-04-15 12:13:07 -0700

    changes to knex file

--- knexfile.js
+++ knexfile.js
@@ -23,12 +23,17 @@ module.exports = {
   development: {
     client: "pg",
     useNullAsDefault: true,
-    connection: {
-      host: process.env.POSTGRESS_DEV_HOST,
-      port: process.env.POSTGRESS_DEV_PORT,
-      user: process.env.POSTGRESS_DEV_USER,
-      password: process.env.POSTGRESS_DEV_PASSWORD,
-      database: process.env.POSTGRESS_DEV_DATABASE
+    // connection: {
+    //   host: process.env.POSTGRESS_TEST_HOST,
+    //   port: process.env.POSTGRESS_TEST_PORT,
+    //   user: process.env.POSTGRESS_TEST_USER,
+    //   password: process.env.POSTGRESS_TEST_PASSWORD,
+    //   database: process.env.POSTGRESS_TEST_DATABASE
+    // },
+    connection: 'postgresql://localhost/mm',
+    pool: {
+      min: 2,
+      max: 10
     },
 
     migrations: {
