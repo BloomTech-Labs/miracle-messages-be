e79dea8f311d34611153dc752a1456261b692853
unknown <viola4lfe@gmail.com>
2020-04-15 13:46:03 -0400

    working errors

--- api/mailRouter.js
+++ api/mailRouter.js
@@ -2,17 +2,17 @@ const express = require("express");
 const router = express.Router();
 const sgMail = require("@sendgrid/mail");
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 router.get("/", async (req, res) => {
-  const msg = {
-    to: "viola4lfe@gmail.com",
-    from: "dan@miraclemessages.org",
-    subject: "Sending with Twilio SendGrid is Fun",
-    text: "and easy to do anywhere, even with Node.js",
-    html: "<strong>and easy to do anywhere, even with Node.js</strong>",
-  };
-  try {
+ const msg = {
+  to: "viola4lfe@gmail.com",
+  from: "dan@miraclemessages.org",
+  subject: "Sending with Twilio SendGrid is Fun",
+  text: "and easy to do anywhere, even with Node.js",
+  html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+ };
+ try {
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
     await sgMail.send(msg);
     res.status(201).json({ message: "mail sent" });
   } catch ({message, stack, code, }) {
