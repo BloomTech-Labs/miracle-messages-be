07c033398b1c0a84b59d33b659dd8419d2abb32d
unknown <viola4lfe@gmail.com>
2020-04-16 16:52:57 -0400

    initial commit for this branch; working testing email automation using a volunteer's id

--- api/mailRouter.js
+++ api/mailRouter.js
@@ -3,34 +3,63 @@ const router = express.Router();
 const sgMail = require("@sendgrid/mail");
 const volunteersDb = require("../models/volunteer-model.js");
 
-sgMail.setApiKey(
-  "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"
-  // "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"
-);
-const axios = require("axios");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-router.post("/", (req, res) => {
-  const msg = {
-    to: "viola4lfe@gmail.com",
-    //TODO need to verify the from account
-    //TODO double check our api_key
-    from: "william@miraclemessages.org",
-    subject: "Sending with Twilio SendGrid is Fun",
-    text: "and easy to do anywhere, even with Node.js",
-    html: "<strong>and easy to do anywhere, even with Node.js</strong>",
-  };
-  //ES6
-  sgMail.send(msg).then(
-    () => {},
-    (error) => {
-      console.error(error);
+router.post("/:id", (req, res) => {
+  console.log("hello", req.body);
+  const  id  = req.body.id;
+  const email = req.body.email;
 
-      if (error.response) {
-        console.error(error.response.body);
-      }
-    }
-  );
+  volunteersDb
+    .findEmail(id)
+    console.log("id", id)
+    // .then(ids => {
+      // console.log("volunteer", ids);
+
+      sgMail
+        .sendMultiple({
+          to: [email],
+          from: "william@miraclemessages.org",
+          templateId: "d-df074a88557646bcbb042df464b7ca6c",
+          subject: "Thank you for your interest in Miracle Messages",
+          // substitutions: {
+          //   comment: req.body.post,
+          // },
+          // text: "Hello",
+          // html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+        })
+        .then((email) => res.status(200).json(email))
+        .catch((err) =>
+          res.status(500).json({ message: "Could not send.", error: err })
+        );
+    // })
+    // .catch(({ message, stack, code }) =>
+    //   res
+    //     .status(500)
+    //     .json({ message: "Could not send email.", error: message, stack, code })
+    // );
 });
+
+// const msg = {
+//   to: "viola4lfe@gmail.com",
+//   //TODO need to verify the from account
+//   //TODO double check our api_key
+//   from: "william@miraclemessages.org",
+//   subject: "Sending with Twilio SendGrid is Fun",
+//   text: "and easy to do anywhere, even with Node.js",
+//   html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+// };
+// //ES6
+// sgMail.send(msg).then(
+//   () => {},
+//   (error) => {
+//     console.error(error);
+
+//     if (error.response) {
+//       console.error(error.response.body);
+//     }
+//   }
+// );
 
 module.exports = router;
 
