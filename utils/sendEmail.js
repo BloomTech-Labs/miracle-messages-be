6614e77e4b8217473320ecb110d09711e7558dac
Alexander Karren <alex@karren.com>
2020-06-02 12:16:15 -0600

    added additional cases for email types

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -9,13 +9,25 @@ module.exports = function sendEmail(type, email) {
     console.log(email);
 
     switch(type) {
+        // to Kevin – chapter POST that needs to be approved
         case "NEW_CHAPTER":
             subject = "A new chapter is awaiting your approval"
             bodyText = "CHANGE LATER - a new chapter is awaiting your approval"
             break;
+        // to Kevin - reunion POST that needs to be approved
         case "NEW_REUNION":
             subject = "A new reunion is awaiting your approval"
             bodyText = "CHANGE LATER - a new chapter is awaiting your approval"
+            break;
+        // to Kevin - someone is requesting to become a chapter leader
+        case "NEW_LEADER":
+            subject = "(NAME HERE) wants to become a chapter leader"
+            bodyText = "CHANGE LATER - someone wants to join your chapter"
+            break;
+        // to chapter leader / Kevin – someone requesting to join chapter
+        case "NEW_MEMBER":
+            subject = "(NAME HERE) wants to join your chapter"
+            bodyText = "CHANGE LATER - someone wants to join your chapter"
             break;
     }
 
@@ -26,7 +38,10 @@ module.exports = function sendEmail(type, email) {
         from: "alex@karren.com",
         subject: `Miracle Map – ${subject}`,
         text: bodyText,
-        html: `<h3>this is what HTML looks like<h3>`
+        html: `
+        <h2>${subject}</h2>
+        <p>${bodyText}<p>
+        `
     })
     .then(() => {
         console.log("Email sent");
