33d3794944258583cabae83dde4bfb97a749bfb3
Ronald McIntyre <r.campbell.mcintyre@gmail.com>
2020-06-03 17:20:57 -0400

    adjusted seeded reunions for error in transpose regex

--- utils/transpose.js
+++ utils/transpose.js
@@ -9,7 +9,7 @@ async function transpose () {
 let arrayOfObjects = array.map(async e => {
   e = e.replace("\n", " ")
 
-  const info = await e.match(/(".*?"|[^",\s]+)(?=\s*,|\s*$)/g)
+  const info = await e.match(/(".*?"|[^",]+)(?=.+\s*|\s*$)/g)
   e = await createObject(info[0],info[1],info[2],info[3],info[4],info[5],info[6],info[7],info[8],info[9],info[10],info[11])
   e.story =  e.story.replace(/"/g,"")
   
