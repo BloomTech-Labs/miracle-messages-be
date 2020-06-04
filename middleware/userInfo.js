6570ff201e0f9c68ca4e7caa5b5e98f8f3a7deb2
Ronald McIntyre <59479270+McIntyre-R@users.noreply.github.com>
2020-06-04 08:25:07 -0400

    Update userInfo.js

--- middleware/userInfo.js
+++ middleware/userInfo.js
@@ -4,7 +4,7 @@ const Axios = require("axios")
 
 
 function userInfo (req,res,next) {
-        console.log("hello")
+
     const config = {headers: {Authorization: "Bearer " + req.accessToken}}
     Axios.get("https://dev-750287.okta.com/oauth2/default/v1/userinfo", config)
     .then(res => {
